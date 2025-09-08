dcaf3446ce57f18a586b3701654dd5e47e454e4e
naim787 <naimmmmab@gmail.com>
2025-09-08 11:21:23 -0400

    Update: 9/8/2025, 11:21:23 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,17 +5,3 @@
   let activeUrl = $derived(page.url.pathname);
 </script>
 
-
-<Navbar class="backdrop-blur">
-  <NavBrand href="/">
-    <span class="self-center whitespace-nowrap dark:text-white font-bold text-2xl bungee-regular">WEBEXPOR</span>
-  </NavBrand>
-  <NavHamburger />
-  <NavUl {activeUrl} >
-    <NavLi href="/" class="text-black bg-white">Home</NavLi>
-    <NavLi href="/" class="text-black bg-white">Navbar</NavLi>
-    <NavLi href="/" class="text-black bg-white">Accordion</NavLi>
-    <NavLi href="/" class="text-black bg-white">Alert</NavLi>
-    <NavLi href="/" class="text-black bg-white">Avatar</NavLi>
-  </NavUl>
-</Navbar>
