7662b491d5e6898b604f57c22211df19b03c8295
naim787 <naimmmmab@gmail.com>
2025-09-08 11:20:43 -0400

    Update: 9/8/2025, 11:20:43 AM

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,13 @@
+<Navbar class="backdrop-blur">
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap dark:text-white font-bold text-2xl bungee-regular">WEBEXPOR</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl {activeUrl} >
+    <NavLi href="/" class="text-black bg-white">Home</NavLi>
+    <NavLi href="/" class="text-black bg-white">Navbar</NavLi>
+    <NavLi href="/" class="text-black bg-white">Accordion</NavLi>
+    <NavLi href="/" class="text-black bg-white">Alert</NavLi>
+    <NavLi href="/" class="text-black bg-white">Avatar</NavLi>
+  </NavUl>
+</Navbar>
