52cd7ecb9ebbcee385fc6dd527ce016cb62dab22
naim787 <naimmmmab@gmail.com>
2025-09-08 10:35:07 -0400

    Update: 9/8/2025, 10:35:07 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,9 +12,9 @@
   <NavHamburger />
   <NavUl {activeUrl}>
     <NavLi href="/">Home</NavLi>
-    <NavLi href="/">Navbar</NavLi>
-    <NavLi href="/">Accordion</NavLi>
-    <NavLi href="/">Alert</NavLi>
-    <NavLi href="/">Avatar</NavLi>
+    <NavLi href="/docs/componentr">Navbar</NavLi>
+    <NavLi href="/docs/components/accordion">Accordion</NavLi>
+    <NavLi href="/docs/components/alert">Alert</NavLi>
+    <NavLi href="/docs/components/avatar">Avatar</NavLi>
   </NavUl>
 </Navbar>
