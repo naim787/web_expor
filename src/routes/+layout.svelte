0fbfa6b6b54a7a4244f7822541d89baecd495c63
naim787 <naimmmmab@gmail.com>
2025-09-08 11:10:43 -0400

    Update: 9/8/2025, 11:10:43 AM

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,8 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Bungee&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bungee&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children?.()}
