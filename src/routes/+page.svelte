81d5e3b0cd3d1b18cd387f12798e42fbf037405d
naim787 <naimmmmab@gmail.com>
2025-09-08 10:32:11 -0400

    Update: 9/8/2025, 10:32:11 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <Navbar>
   <NavBrand href="/">
-    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
   </NavBrand>
   <NavHamburger />
   <NavUl {activeUrl}>
