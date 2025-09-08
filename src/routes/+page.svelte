06c61b7fee6842b734a58ee9089025b9b3903a7a
naim787 <naimmmmab@gmail.com>
2025-09-08 11:21:17 -0400

    Update: 9/8/2025, 11:21:17 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { page } from "$app/state";
+  import Nav from "$lib/components/Nav.svelte";
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
-  
   let activeUrl = $derived(page.url.pathname);
 </script>
 
