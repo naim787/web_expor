278003f68d536e9b599a1274053d640661251c07
naim787 <naimmmmab@gmail.com>
2025-09-08 10:51:57 -0400

    Update: 9/8/2025, 10:51:57 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <Navbar>
   <NavBrand href="/">
-    <span class="self-center whitespace-nowrap dark:text-white font-bold text-2xl">WEBEXPOR</span>
+    <span class="self-center whitespace-nowrap dark:text-white font-bold text-2xl tex">WEBEXPOR</span>
   </NavBrand>
   <NavHamburger />
   <NavUl {activeUrl} >
