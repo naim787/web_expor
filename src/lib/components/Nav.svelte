3dd107bcb2eab829a4007b70291149806aa602c5
naim787 <naimmmmab@gmail.com>
2025-09-08 11:24:43 -0400

    Update: 9/8/2025, 11:24:43 AM

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,7 +4,7 @@
     let activeUrl = $derived(page.url.pathname);
 </script>
 
-<Navbar class="backdrop-blur border-b-[]">
+<Navbar class="backdrop-blur border-b-[2]">
   <NavBrand href="/">
     <span class="self-center whitespace-nowrap dark:text-white font-bold text-2xl bungee-regular">WEBEXPOR</span>
   </NavBrand>
