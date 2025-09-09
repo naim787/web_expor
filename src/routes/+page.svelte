1263c03e18cdb3e58e322b3e4bd6d07ea0b38399
naim787 <naimmmmab@gmail.com>
2025-09-09 13:00:37 -0400

    Update: 9/9/2025, 1:00:37 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,7 @@
   import { Button, GradientButton, Avatar} from "flowbite-svelte";
 
   import { Carousel, Controls, CarouselIndicators, Thumbnails } from "flowbite-svelte";
-  let images = {
-      alt: "Cosmic timetraveler",
-      src: "/images/carousel/cosmic-timetraveler-pYyOZ8q7AII-unsplash.webp",
-      title: "cosmic-timetraveler-pYyOZ8q7AII-unsplash.com"
-    }
+  let images = 
   let index = $state(0);
 
   //device
