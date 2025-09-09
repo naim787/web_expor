07e39295ba485ab33a9ed05c45e6df6bdc14bcce
naim787 <naimmmmab@gmail.com>
2025-09-09 06:11:24 -0400

    Update: 9/9/2025, 6:11:24 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,15 @@
 <div class="w-[100vw] h-[100vh] dark:bg-gray-950 text-black p-4 dark:text-white">
 
   <!-- section -->
-   <div class="w-[100vw] h-[100v]">
-    <div class="">
-      <h1 class="bungee-regular text-5xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
-      <div class="flex">
-        <Avatar src="https://images.nightcafe.studio/jobs/REDfxKehp6X24LqqFDwj/REDfxKehp6X24LqqFDwj--0--kydh4.jpg?tr=w-1600,c-at_max" stacked />
-        <Avatar src="https://images.nightcafe.studio/jobs/7wY35HmH7kDK5IWkXM53/7wY35HmH7kDK5IWkXM53--0--x64ol.jpg?tr=w-1600,c-at_max" stacked />
-        <Avatar src="https://images.nightcafe.studio/jobs/Hz3hy7nuh0EB0JEdOKG0/Hz3hy7nuh0EB0JEdOKG0--0--79lhe.jpg?tr=w-1600,c-at_max" stacked />
-    <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+5230</Avatar>
-
-        </div>
-    </div>
+   <div class="w-"></div>
+  <div class="pt-25 w-120">
+    <h1 class="bungee-regular text-5xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
+    <div class="flex">
+      <Avatar src="https://images.nightcafe.studio/jobs/REDfxKehp6X24LqqFDwj/REDfxKehp6X24LqqFDwj--0--kydh4.jpg?tr=w-1600,c-at_max" stacked />
+      <Avatar src="https://images.nightcafe.studio/jobs/7wY35HmH7kDK5IWkXM53/7wY35HmH7kDK5IWkXM53--0--x64ol.jpg?tr=w-1600,c-at_max" stacked />
+      <Avatar src="https://images.nightcafe.studio/jobs/Hz3hy7nuh0EB0JEdOKG0/Hz3hy7nuh0EB0JEdOKG0--0--79lhe.jpg?tr=w-1600,c-at_max" stacked />
+  <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+5230</Avatar>
+</div>
   </div>
 
 </div>
