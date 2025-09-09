d4e6c0696d8746c3ca6722467e735ec894792871
naim787 <naimmmmab@gmail.com>
2025-09-09 12:07:44 -0400

    Update: 9/9/2025, 12:07:44 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,7 +64,7 @@
             <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+5230</Avatar>
 
             {:else}
-              </div>
+        </div>
           <Skeleton size="sm" class="my-8" />
             {/if}
         </div>
