0021e2e3e33d72e25587795001369b6ba7fa5d71
naim787 <naimmmmab@gmail.com>
2025-09-09 04:58:05 -0400

    Update: 9/9/2025, 4:58:05 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,6 @@
 <Nav />
 
 <div class="w-[100vw] h-[100vh] dark:bg-gray-950 text-black pt-15">
+  <div class=""></div>
   <h1 class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quam necessitatibus vero itaque eligendi atque.</h1>
 </div>
