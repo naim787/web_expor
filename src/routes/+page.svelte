870d3fae4731cb3b43a3d0bccbfce639c7b91ab4
naim787 <naimmmmab@gmail.com>
2025-09-09 02:42:11 -0400

    Update: 9/9/2025, 2:42:11 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,6 +27,7 @@
     <!-- // jusudul -->
     <div class="w-100">
        <h1 class="text-5xl font-bold bungee-regular">Mulailah Deggan Lagkah <span class="text-orange-500">Besar</span> Tanpa Biaya Besar</h1>
+       
     </div>
   
     <!-- tremplate gerak -->
