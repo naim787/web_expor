9ccae984f171df22ad8630de83471cdddb92d057
naim787 <naimmmmab@gmail.com>
2025-09-09 12:39:19 -0400

    Update: 9/9/2025, 12:39:19 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,7 +71,7 @@
 
         <DeviceMockup device="laptop">
           <div class="w-150 h-200 border bg-red-500 relative">
-            <div class="w-full h-10 bg-black flex items-center j">
+            <div class="w-full h-10 bg-black flex items-center justify-between">
               <span class="self-center whitespace-nowrap dark:text-white font-bold text-md bungee-regular">WEBEXPOR</span>
             </div>
           </div>
