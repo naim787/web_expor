896b0aa7de12ca6e4de2f00a08974442ffe653f3
naim787 <naimmmmab@gmail.com>
2025-09-09 11:39:43 -0400

    Update: 9/9/2025, 11:39:43 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,15 +43,15 @@
   
     <!-- section -->
      <div class="w-full h-[100vh] flex justify-center items-center">
-      <Skeleton size="sm" class="my-8" />
        <div class="w-120" transition:fade>
          <h1 class="bungee-regular text-5xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
          <div class="flex">
            <Avatar src="https://images.nightcafe.studio/jobs/REDfxKehp6X24LqqFDwj/REDfxKehp6X24LqqFDwj--0--kydh4.jpg?tr=w-1600,c-at_max" stacked />
            <Avatar src="https://images.nightcafe.studio/jobs/7wY35HmH7kDK5IWkXM53/7wY35HmH7kDK5IWkXM53--0--x64ol.jpg?tr=w-1600,c-at_max" stacked />
            <Avatar src="https://images.nightcafe.studio/jobs/Hz3hy7nuh0EB0JEdOKG0/Hz3hy7nuh0EB0JEdOKG0--0--79lhe.jpg?tr=w-1600,c-at_max" stacked />
-         <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+5230</Avatar>
-        </div>
+           <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+5230</Avatar>
+          </div>
+          <Skeleton size="sm" class="my-8" />
        </div>
      </div>
   
