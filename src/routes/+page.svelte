7d1c297bb663ece2c439b0e8759c7c8e9744fc9c
naim787 <naimmmmab@gmail.com>
2025-09-09 05:01:43 -0400

    Update: 9/9/2025, 5:01:43 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <Nav />
 
-<div class="w-[100vw] h-[100vh] dark:bg-gray-950 text-black p-2 ">
+<div class="w-[100vw] h-[100vh] dark:bg-gray-950 text-black p-2 dark:text-white">
   <div class="pt-22 w-100">
     <h1 class="bungee-regular">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quam necessitatibus vero itaque eligendi atque.</h1>
   </div>
