db7d69145d5eeaa58e4e14340cd2715a880cc106
naim787 <naimmmmab@gmail.com>
2025-09-09 12:19:51 -0400

    Update: 9/9/2025, 12:19:51 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,8 +70,7 @@
         </div>
 
         <DeviceMockup device="laptop">
-          <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/laptop-screen.png" class="h-[156px] w-full rounded-xl md:h-[278px] dark:hidden" alt="laptop example 1" />
-          <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/laptop-screen-dark.png" class="hidden h-[156px] w-full rounded-lg md:h-[278px] dark:block" alt="laptop example 2" />
+         
         </DeviceMockup>
      </div>
   
