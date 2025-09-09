8dd4665aaec1b22f81d13af0c82254819286a958
naim787 <naimmmmab@gmail.com>
2025-09-09 03:25:56 -0400

    Update: 9/9/2025, 3:25:56 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
   import Nav from "$lib/components/Nav.svelte";
   import { onMount } from "svelte";
   import "../app.css";
-  import { Gallery, Label, Input } from "flowbite-svelte";
+  import { Gallery , Label, Input} from "flowbite-svelte";
   import { SearchOutline } from "flowbite-svelte-icons";
+  //  <SearchOutline class="shrink-0 h-6 w-6" />
 
   const images = [
     { alt: "shoes", src: "./tmp0.webp" },
@@ -11,25 +12,37 @@
     { alt: "plants", src: "./tmp1.webp" },
     { alt: "watch", src: "./tmp0.webp" }
   ];
-
+  
   let Domain = "";
-
-  onMount(async () => {
+  onMount( async() => {
     const res = await fetch("/?domain=webexpor.com");
     const data = await res.json();
     console.log(JSON.stringify(data)); // Tampilkan data sebagai string JSON
     console.table(data); // Tampilkan data dalam format tabel
   });
+  
 </script>
 
 <Nav />
 
 <div class="dark:bg-gray-950 dark:text-white w-[100vw] h-[100vh] p-2">
+
   <div class="w-full justify-evenly items-center flex pt-[15vh]">
+    <!-- // jusudul -->
     <div class="w-100">
-      <h1 class="text-5xl font-bold bungee-regular">Mulailah Deggan Lagkah <span class="text-orange-500">Besar</span> Tanpa Biaya Besar</h1>
-      <Input id="text" type="text" placeholder="Cari Domain" class="pl-8" bind:value={Domain} />
+       <h1 class="text-5xl font-bold bungee-regular">Mulailah Deggan Lagkah <span class="text-orange-500">Besar</span> Tanpa Biaya Besar</h1>
+       <Input id="text" type="text" placeholder="Cari Domain" class="pl-8" bind:value={Domain}>
+        {#snippet left()}
+        <SearchOutline class="shrink-0 h-6 w-6" />
+        {/snippet}
+      </Input>
+      <h1 class="text-red-500 text-2xl">{Domain}</h1>
     </div>
+  
+    <!-- tremplate gerak -->
+     <!-- <div class="w-200 h-60 border">
+      <img src="./tmp0.webp" alt="" class="w-60">
+     </div> -->
     <Gallery class="grid-cols-2 gap-2 w-100" items={images} />
   </div>
 </div>
