14bccfdfcc12eaa64ee996eef56dbff532b30d48
naim787 <naimmmmab@gmail.com>
2025-09-09 02:31:58 -0400

    Update: 9/9/2025, 2:31:58 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,6 +39,6 @@
     </div>
   </div>
   
-  <div class="w-[20vw] h-[20vw] absolute top-0 left-0 bg-blue-800 rounded-full z-[-1]"></div>
+  <div class="w-[20vw] h-[20vw] absolute top-0 left-0 bg-blue-800 rounded-full"></div>
 
 </div>
