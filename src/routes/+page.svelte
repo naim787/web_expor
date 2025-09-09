1849859e4f3e482cdee2c87d15cc8800b02dc224
naim787 <naimmmmab@gmail.com>
2025-09-09 12:40:58 -0400

    Update: 9/9/2025, 12:40:58 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,7 +73,9 @@
           <div class="w-150 h-200 border bg-red-500 relative">
             <div class="w-full h-10 bg-black flex items-center justify-between p-2">
               <span class="self-center whitespace-nowrap dark:text-white font-bold text-md bungee-regular">WEBEXPOR</span>
-              <div class="w-50"></div>
+              <div class="w-50">
+                
+              </div>
               <a href="/" class="">Pertanyaan</a>
               <a href="/" class="">Template</a>
               <a href="/" class="">Tutorial</a>
