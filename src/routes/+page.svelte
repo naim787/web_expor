e1ed826ef5b575f7251aaabead3efc3e37e73167
naim787 <naimmmmab@gmail.com>
2025-09-09 12:55:20 -0400

    Update: 9/9/2025, 12:55:20 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,7 +82,7 @@
               </div>
             </div>
 
-            <div class="flex justify-evenly gap-2">
+            <div class="flex justify-evenly gap-2 fle">
               <img src="./tmp0.webp" alt="" class="w-20">
             </div>
           </div>
