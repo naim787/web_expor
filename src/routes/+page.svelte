4cfef343a06617be9537b2bae9b70a853c125cb8
naim787 <naimmmmab@gmail.com>
2025-09-09 11:42:15 -0400

    Update: 9/9/2025, 11:42:15 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,56 @@
-.................
+<script>
+  import Nav from "$lib/components/Nav.svelte";
+  import "../app.css"
+  import { Button, GradientButton, Avatar} from "flowbite-svelte";
+
+  import { onMount, onDestroy } from "svelte";
+
+  // Skeleton
+  import { Skeleton } from "flowbite-svelte";
+
+  // Transition
+  import { fade } from 'svelte/transition';
+
+  let scrollEl;
+  let scrollInstance;
+
+  onMount(async () => {
+    const LocomotiveScroll = (await import("locomotive-scroll")).default;
+
+    // Wait until scrollEl is ready
+    if (scrollEl) {
+      scrollInstance = new LocomotiveScroll({
+        el: scrollEl,
+        smooth: true,
+        smoothMobile: false,
+        getSpeed: true,
+        getDirection: true
+      });
+    }
+  });
+
+  onDestroy(() => {
+    // Important! Destroy the instance to prevent crashes
+    if (scrollInstance) {
+      scrollInstance.destroy();
+    }
+  });
+</script>
+
+<Nav />
+
+<div class="w-[100vw] h-auto dark:bg-gray-950 text-black p-4 dark:text-white overflow-hidden" bind:this={scrollEl} data-scroll-container>
+  <!-- section -->
+  <div class="w-full h-[100vh] flex justify-center items-center">
+    <div class="w-120" transition:fade>
+      <Skeleton size="h1" class="mb-4" />
+      <div class="flex">
+        <Skeleton size="avatar" class="mr-2" />
+        <Skeleton size="avatar" class="mr-2" />
+        <Skeleton size="avatar" class="mr-2" />
+        <Skeleton size="avatar" class="mr-2" />
+      </div>
+      <Skeleton size="sm" class="my-8" />
+    </div>
+  </div>
+</div>
