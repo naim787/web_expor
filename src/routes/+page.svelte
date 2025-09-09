d342dcc40b1c666c98a69a928ee3050e9b480630
naim787 <naimmmmab@gmail.com>
2025-09-09 05:13:28 -0400

    Update: 9/9/2025, 5:13:28 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <Nav />
 
 <div class="w-[100vw] h-[100vh] dark:bg-gray-950 text-black p-2 dark:text-white">
-  <div class="pt-30 w-[50vw]">
+  <div class="pt-30 w-130">
     <h1 class="bungee-regular text-3xl">Tingkatkan Visibilitas Bisnis Anda secara <span class="text-yellow-500">Global</span> dengan <span class="text-orange-500">Website</span> Profesional</h1>
   </div>
 </div>
