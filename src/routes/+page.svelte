a4770a4d55f9e7de616f2af6769e38f17a8946e7
naim787 <naimmmmab@gmail.com>
2025-09-09 05:50:21 -0400

    Update: 9/9/2025, 5:50:21 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,12 @@
   <!-- section -->
   <div class="pt-25 w-120">
     <h1 class="bungee-regular text-3xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
+    <div class="flex">
+  <Avatar src="/images/profile-picture-1.webp" stacked />
+  <Avatar src="/images/profile-picture-2.webp" stacked />
+  <Avatar src="/images/profile-picture-3.webp" stacked />
+  <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+99</Avatar>
+</div>
   </div>
 
 </div>
