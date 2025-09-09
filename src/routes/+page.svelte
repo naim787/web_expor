5bfcb98463b989ea18b8a598d0098c8e87ad6d24
naim787 <naimmmmab@gmail.com>
2025-09-09 12:35:17 -0400

    Update: 9/9/2025, 12:35:17 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,7 +71,7 @@
 
         <DeviceMockup device="laptop">
           <div class="w-150 h-200 border bg-red-500 relative">
-            <div class="w-full"></div>
+            <div class="w-full h-20 bg-black"></div>
           </div>
         </DeviceMockup>
      </div>
