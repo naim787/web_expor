3bb2c72a3792882ad1111c3e27b832585a7494a9
naim787 <naimmmmab@gmail.com>
2025-09-09 05:46:01 -0400

    Update: 9/9/2025, 5:46:01 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
   <!-- section -->
   <div class="pt-25 w-120">
-    <h1 class="bungee-regular text-3xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <span class="bg-linear-65 from-pink-500 to-orange-500 px-1 rounded-md text-white">Website</span> <GradientButton shadow color="red" class="">Website</GradientButton> Terjangkau</h1>
+    <h1 class="bungee-regular text-3xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <span class="bg-linear-65 from-pink-500 to-orange-500 px-1 rounded-md text-white">Website</span> <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
   </div>
 
 </div>
