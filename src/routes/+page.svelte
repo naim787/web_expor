1955fb575165da683b629d0c70c54334a68e0b4a
naim787 <naimmmmab@gmail.com>
2025-09-09 12:36:36 -0400

    Update: 9/9/2025, 12:36:36 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,7 +71,7 @@
 
         <DeviceMockup device="laptop">
           <div class="w-150 h-200 border bg-red-500 relative">
-            <div class="w-full h-10 ">
+            <div class="w-full h-10">
               <NavHamburger />
             </div>
           </div>
