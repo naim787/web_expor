ce57bca78d0d305f86c6e37b02154599f52136a6
naim787 <naimmmmab@gmail.com>
2025-09-09 02:57:25 -0400

    Update: 9/9/2025, 2:57:25 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import { onMount } from "svelte";
   import "../app.css";
   import { Gallery , Label, Input} from "flowbite-svelte";
-  import { Sea } from "flowbite-svelte-icons";
+  import { SearchOutline } from "flowbite-svelte-icons";
   //  <SearchOutline class="shrink-0 h-6 w-6" />
   
   const images = [
