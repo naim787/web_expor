6c2e8688f5f25768f0bf125a5f7939bcbbd5a646
naim787 <naimmmmab@gmail.com>
2025-09-09 12:59:35 -0400

    Update: 9/9/2025, 12:59:35 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import { Button, GradientButton, Avatar} from "flowbite-svelte";
 
   import { Carousel, Controls, CarouselIndicators, Thumbnails } from "flowbite-svelte";
-  let images 
+  let images = []
 
   let index = $state(0);
 
