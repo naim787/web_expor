43bb163ecdbb4c649f246d9c85430b187960106c
naim787 <naimmmmab@gmail.com>
2025-09-09 13:21:56 -0400

    Update: 9/9/2025, 1:21:56 PM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,15 +82,7 @@
               </div>
             </div>
 
-            <div class="flex justify-evenly gap-2 flex-wrap">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
-              <img src="./tmp0.webp" alt="" class="w-20">
+            <div class="flex justify-evenly gap-2 fle">
               <img src="./tmp0.webp" alt="" class="w-20">
             </div>
           </div>
