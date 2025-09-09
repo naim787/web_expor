84b03b34afe1e17559012646c857a836cf0eda1d
naim787 <naimmmmab@gmail.com>
2025-09-09 02:40:06 -0400

    Update: 9/9/2025, 2:40:06 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,23 +21,21 @@
 
 <Nav />
 
-<div class="w-auto h-auto dark:bg-gray-950">
+<div class="w-auto h-auto dark:bg-gray-950"></div>
+<div class="dark:text-white w-[100vw] h-[100vh] p-2 z-10 backdrop-blur">
 
-  <div class="dark:text-white w-[100vw] h-[100vh] p-2 z-10 backdrop-blur">
-  
-    <div class="w-full justify-evenly items-center flex pt-[15vh]">
-      <!-- // jusudul -->
-      <div class="w-100">
-         <h1 class="text-5xl font-bold bungee-regular">Mulailah Deggan Lagkah <span class="text-orange-500">Besar</span> Tanpa Biaya Besar</h1>
-      </div>
-    
-      <!-- tremplate gerak -->
-       <!-- <div class="w-200 h-60 border">
-        <img src="./tmp0.webp" alt="" class="w-60">
-       </div> -->
-      <Gallery class="grid-cols-2 gap-2 w-100" items={images} />
+  <div class="w-full justify-evenly items-center flex pt-[15vh]">
+    <!-- // jusudul -->
+    <div class="w-100">
+       <h1 class="text-5xl font-bold bungee-regular">Mulailah Deggan Lagkah <span class="text-orange-500">Besar</span> Tanpa Biaya Besar</h1>
     </div>
-  </div>
   
-  <div class="w-[20vw] h-[20vw] absolute top-0 left-0 bg-blue-800 rounded-full z-[-1]"></div>
+    <!-- tremplate gerak -->
+     <!-- <div class="w-200 h-60 border">
+      <img src="./tmp0.webp" alt="" class="w-60">
+     </div> -->
+    <Gallery class="grid-cols-2 gap-2 w-100" items={images} />
+  </div>
 </div>
+
+<div class="w-[20vw] h-[20vw] absolute top-0 left-0 bg-blue-800 rounded-full z-[-1]"></div>
