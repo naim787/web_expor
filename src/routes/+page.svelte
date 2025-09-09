090bc8e84caa9b1050fffafacb8269c5a5406167
naim787 <naimmmmab@gmail.com>
2025-09-09 07:31:56 -0400

    Update: 9/9/2025, 7:31:56 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,7 @@
   
     <!-- section -->
      <div class="w-full h-[100vh] flex justify-center items-center">
-       <div class="w-120">
+       <div class="w-120" transition:fade>
          <h1 class="bungee-regular text-5xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
          <div class="flex">
            <Avatar src="https://images.nightcafe.studio/jobs/REDfxKehp6X24LqqFDwj/REDfxKehp6X24LqqFDwj--0--kydh4.jpg?tr=w-1600,c-at_max" stacked />
