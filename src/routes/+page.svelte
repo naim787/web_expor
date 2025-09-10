75053d6ca146f70fe65286e98f416d24b78565dc
naim787 <naimmmmab@gmail.com>
2025-09-10 03:08:59 -0400

    Update: 9/10/2025, 3:08:59 AM

---
+++ src/routes/+page.svelte
@@ -0,0 +1,124 @@
+<script>
+  import Nav from "$lib/components/Nav.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import "../app.css"
+
+  import { Button, GradientButton, Avatar} from "flowbite-svelte";
+  //device
+  import { DeviceMockup } from "flowbite-svelte";
+
+  import { onMount, onDestroy } from "svelte";
+
+// skeleton
+  import { Skeleton } from "flowbite-svelte";
+  let bannerLabel;
+// transation
+  import { fade } from 'svelte/transition';
+
+  let scrollEl;
+  let scrollInstance;
+
+  onMount(async () => {
+    const LocomotiveScroll = (await import("locomotive-scroll")).default;
+
+    // Tunggu hingga scrollEl siap
+    if (scrollEl) {
+      // scrollInstance = new LocomotiveScroll({
+      //   el: scrollEl,
+      //   smooth: true,
+      //   smoothMobile: false,
+      //   getSpeed: true,
+      //   getDirection: true
+      // });
+      scrollInstance = new LocomotiveScroll({
+        el: scrollEl,
+        smooth: true,
+        smoothMobile: true,
+        getSpeed: true,
+        getDirection: true,
+        scrollFrom: "top",
+        scrollOffset: 50,
+        multiplier: 0.1
+      });
+    }
+  });
+
+  onDestroy(() => {
+    // Penting! Hancurkan instance agar tidak crash
+    if (scrollInstance) {
+      scrollInstance.destroy();
+    }
+  });
+  onMount(async() => {
+      bannerLabel = await [
+       "https://images.nightcafe.studio/jobs/REDfxKehp6X24LqqFDwj/REDfxKehp6X24LqqFDwj--0--kydh4.jpg?tr=w-1600,c-at_max",
+       "https://images.nightcafe.studio/jobs/7wY35HmH7kDK5IWkXM53/7wY35HmH7kDK5IWkXM53--0--x64ol.jpg?tr=w-1600,c-at_max",
+       "https://images.nightcafe.studio/jobs/Hz3hy7nuh0EB0JEdOKG0/Hz3hy7nuh0EB0JEdOKG0--0--79lhe.jpg?tr=w-1600,c-at_max"
+     ];
+  });
+
+</script>
+
+<div class="dark:bg-gray-950 text-black dark:text-white overflow-hidden">
+    <Nav />
+    <div class="w-[100vw] h-auto p-4 " bind:this={scrollEl} data-scroll-container>
+    
+      <!-- section -->
+       <div class="w-full h-[100vh] flex justify-center items-center flex-col md:flex-row">
+         <div class="w-120" transition:fade>
+           {#if bannerLabel}
+           <h1 class="bungee-regular text-5xl">Percepat Transformasi Digital Bisnis Anda dengan Solusi <GradientButton shadow color="red" class="text-3xl">Website</GradientButton> Terjangkau</h1>
+           <div class="flex">
+              {#each bannerLabel as banner}
+                <Avatar src={banner} stacked />
+              {/each}
+              <Avatar stacked href="/" class="bg-gray-700 text-sm text-white hover:bg-gray-600">+5230</Avatar>
+    
+            </div>
+              {:else}
+              <Skeleton size="sm" class="my-8" />
+            {/if}
+          </div>
+    
+          <DeviceMockup device="laptop">
+            <div class="w-150 h-200 bg-gray-950 relative text-white">
+              <!-- nav -->
+              <div class="w-full h-8 bg-black flex justify-between items-center p-2 shadow-2xl shadow-white/20">
+                <div class="self-center font-bold text-[15px] bungee-regular">WEBEXPOR</div>
+                <div class="w-70 text-[10px]">
+                  <a href="/" class="">Pertanyaan</a>
+                  <a href="/" class="">Template</a>
+                  <a href="/" class="">Tutorial</a>
+                  <a href="/" class="">Tentag Kami</a>
+                </div>
+              </div>
+    
+              <div class="flex justify-evenly gap-2 flex-wrap">
+                <img src="./tmp0.webp" alt="" class="w-20">
+              </div>
+            </div>
+          </DeviceMockup>
+       </div>
+    
+       <!-- layer 2 -->
+       <div class="w-full h-auto dark:bg-gray-950">
+         <div class="w-full  md:w-[90vw] h-50 md:dark:bg-black md:inset-shadow-sm md:inset-shadow-white/20 flex items-center md:rounded-r-full absolute left-0 p-2">
+          <h1 class="bungee-regular text-5xl">Pilih Desainmu</h1>
+         </div>
+         <div class="w-full h-auto flex flex-wrap  gap-2 justify-evenly md:w-[80%] pt-55 pb-4 dark:md:bg-gray-900 dark:md:rounded-b-4xl">
+          <div class="w-120 h-70  bg-black inset-shadow-sm inset-shadow-white/50 rounded-md"></div>
+          <div class="w-120 h-70  bg-black inset-shadow-sm inset-shadow-white/50 rounded-md"></div>
+          <div class="w-120 h-70  bg-black inset-shadow-sm inset-shadow-white/50 rounded-md"></div>
+          <div class="w-120 h-70  bg-black inset-shadow-sm inset-shadow-white/50 rounded-md"></div>
+          <div class="w-120 h-70  bg-black inset-shadow-sm inset-shadow-white/50 rounded-md"></div>
+          <div class="w-120 h-70  bg-black inset-shadow-sm inset-shadow-white/50 rounded-md"></div>
+          <div class="w-full flex justify-center p-2">
+            <GradientButton shadow color="pink">Lihat Semua</GradientButton>
+          </div>
+         </div>
+       </div>
+    
+    </div>
+    <Footer />
+</div>
+  
